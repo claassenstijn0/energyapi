d3aec86c96c14a2f76c4d7861584b76ee3b69146
Stijn Claassen <118205462+claassenstijn0@users.noreply.github.com>
2025-05-27 14:19:56 +0200

    Update S3Service.java
    Added canary token

--- adminpanel/app/src/main/java/com/energy/admin/service/S3Service.java
+++ adminpanel/app/src/main/java/com/energy/admin/service/S3Service.java
@@ -16,8 +16,8 @@ public class S3Service {
     private final S3Client s3Client;
 
     public S3Service() {
-        String aws_access_key_id = "AKIA<placeholder>";
-        String aws_secret_access_key = "<placeholder>";
+        String aws_access_key_id = "AKIAUM4G6O6NBKBLMA7L";
+        String aws_secret_access_key = "b0NDvSNWkJjPfVQIRiL8H6plOReAC8WXNkALrXhZ";
  
         AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(aws_access_key_id, aws_secret_access_key);
 
